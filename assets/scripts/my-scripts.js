213a796b056c7e504560b03ca2bf7f8ec637a882
Sarah Duitsman <sksmart9@gmail.com>
2016-05-01 16:23:41 -0500

    script updates

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -6,22 +6,20 @@ $(document).ready(function() {
 	});
 
 	$('#show-menu-btn').click(function () {
-		$('#menu').show();
-	});
-
-	$('#hide-menu-btn').click(function () {
-		$('#menu').hide();
-	});
-
-	//CSS highlight class can be found in assets/styles/main.css
-	$('.checkbox-group').on('click', 'input:checkbox', function () {
-    console.log(this.checked);
-	 	$(this).parent().toggleClass('highlight'); 
+    $('#menu').show();
   });
 
- 	$('#order-btn').click(function() {
- 		var orderName = $("input:radio:checked").val();
- 		$('#order-details').html("<h1>" + orderName + "'s order is ready!</h1>");
- 	});
+  $('#hide-menu-btn').click(function () {
+    $('#menu').hide();
+  });
 
+  //CSS highlight class can be found in assets/styles/main.css
+  $('input[type="checkbox"]').click(function(){
+    $(this).parent().toggleClass('highlight'); 
+  });
+
+  $('#order-btn').click(function() {
+    var orderName = $("input:radio:checked").val();
+    $('#order-details').html("<h1>" + orderName + "'s order is ready!</h1>");
+  });
 });
