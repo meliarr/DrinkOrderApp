f10547f47c4f17be80611c4b0efb4c6d4f87f2cc
Sarah Duitsman <sksmart9@gmail.com>
2016-04-30 20:36:42 -0500

    final key

---
+++ assets/scripts/my-scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$('.bxslider').bxSlider({
+	  mode: 'vertical',
+	  captions: true,
+	  slideWidth: 500
+	});
+
+	$('#show-menu-btn').click(function () {
+		$('#menu').show();
+	});
+
+	$('#hide-menu-btn').click(function () {
+		$('#menu').hide();
+	});
+
+	//CSS highlight class can be found in assets/styles/main.css
+	$('.checkbox-group').on('click', 'input:checkbox', function () {
+    console.log(this.checked);
+	 	$(this).parent().toggleClass('highlight'); 
+  });
+
+ 	$('#order-btn').click(function() {
+ 		var orderName = $("input:radio:checked").val();
+ 		$('#order-details').html("<h1>" + orderName + "'s order is ready!</h1>");
+ 	});
+
+});
