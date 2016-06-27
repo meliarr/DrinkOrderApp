a5766e3974a4a5f12370d2556b38a035ed79236a
Kayla Hennegin <kayla@bellafaithdesign.com>
2016-06-26 19:39:49 -0500

    rough project key

---
+++ assets/scripts/drinks-data.js
@@ -0,0 +1,34 @@
+var cocktails = [
+  {
+    'value': 'focusedLady',
+    'label': 'Focused Lady'
+  },
+  {
+    'value': 'strongLady',
+    'label': 'Strong Lady'
+  },
+  {
+    'value': 'frontEndPunch',
+    'label': 'Front-End Punch'
+  },
+  {
+    'value': 'cachedOut',
+    'label': 'Cached Out'
+  },
+  {
+    'value': 'httPapaya',
+    'label': 'httPAPAYA://'
+  },
+  {
+    'value': 'nerdyDaiquiri',
+    'label': 'Nerdy Daiquiri'
+  },
+  {
+    'value': 'theAvernaCode',
+    'label': 'The Averna Code'
+  },
+  {
+    'value': 'focusedTheMostest',
+    'label': 'Focused the Mostest'
+  },
+];
--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -1,4 +1,51 @@
+var cocktails = [
+  {
+    'value': 'focusedLady',
+    'label': 'Focused Lady'
+  },
+  {
+    'value': 'strongLady',
+    'label': 'Strong Lady'
+  },
+  {
+    'value': 'frontEndPunch',
+    'label': 'Front-End Punch'
+  },
+  {
+    'value': 'cachedOut',
+    'label': 'Cached Out'
+  },
+  {
+    'value': 'httPapaya',
+    'label': 'httPAPAYA://'
+  },
+  {
+    'value': 'nerdyDaiquiri',
+    'label': 'Nerdy Daiquiri'
+  },
+  {
+    'value': 'theAvernaCode',
+    'label': 'The Averna Code'
+  },
+  {
+    'value': 'focusedTheMostest',
+    'label': 'Focused the Mostest'
+  },
+];
+
+var loadMenu = function() {
+  for (var i = 0; i < cocktails.length; i++) {
+    $('.checkbox-group').append(
+      '<label class="checkbox" id="' + cocktails[i].value + '"><input type="checkbox" value="' + cocktails[i].value + '">' + cocktails[i].label + '</label>'
+    );
+  }
+};
+
+//get drinks-data via ajax?
+
 $(document).ready(function() {
+  loadMenu();
+
 	$('.bxslider').bxSlider({
 	  mode: 'vertical',
 	  captions: true,
@@ -20,6 +67,18 @@ $(document).ready(function() {
 
   $('#order-btn').click(function() {
     var orderName = $("input:radio:checked").val();
-    $('#order-details').html("<h1>" + orderName + "'s order is ready!</h1>");
+    if (orderName) {
+      $('#order-details').html("<h1>" + orderName + "'s order is ready!</h1>");
+    }
+    else {
+      alert('Oops! Please select your name to order your drink.');
+    }
   });
 });
+
+var submitOrder = function() {
+  var orderName = $("input:radio:checked").val();
+  $('#order-details').html("<h1>" + orderName + "'s order is ready!</h1>");
+};
+
+// 'Thank you, '+ name + ' for ordering ' + drinks + '. We'll let you know when your drink(s) is/are ready!'
