6d8276f53297f7a9589a7b06200306d3f1460e04
Sarah Duitsman <sksmart9@gmail.com>
2016-07-02 13:13:31 -0500

    Starting point for programming fundamentals

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -1,5 +1,4 @@
 //initialize order count
-var orderCount = 0;
 
 /* array of drink objects
    each object has a value key with a camelCase drink name value 
@@ -9,24 +8,13 @@ var orderCount = 0;
 
 
 // submitOrder function that increments orderCount, gathers and displays order details.
-var submitOrder = function() {
-  orderCount++;
-  var orderName = $("#order-form-input").val();
-  console.log(orderName);
-  var drinkName = $("label[for='" + $("input[type='radio']:checked").attr("id") + "']").text();
   
   // only add orders if there is room left on the order queue
-  if (orderCount <= 5) {
-    // add order display
-    $("#order-details").append("<h1>" + orderName + " would like a " + drinkName + "</h1>");
 
-    // update drink order count
-    $('#drink-count').html("Drinks Ordered: " + orderCount);
-  }
-  else {
-    alert('Drink order queue is full.  Please try ordering again in a few minutes.');
-  }
-};
+  // add order display
+
+  // update drink order count
+
 
 
 $(document).ready(function() {
@@ -34,6 +22,5 @@ $(document).ready(function() {
 
   $('#order-btn').click(function() {
     // call submitOrder function when order button is clicked
-    submitOrder();
   });
 });
