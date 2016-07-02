092fa4816cb95e23300f868360ba88042e0d54c6
Sarah Duitsman <sksmart9@gmail.com>
2016-07-02 13:02:59 -0500

    part one answer key

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -4,49 +4,9 @@ var orderCount = 0;
 /* array of drink objects
    each object has a value key with a camelCase drink name value 
    and a label key with the display text for the drink */
-var cocktails = [
-  {
-    'value': 'focusedLady',
-    'label': 'Focused Lady'
-  },
-  {
-    'value': 'strongLady',
-    'label': 'Strong Lady'
-  },
-  {
-    'value': 'frontEndPunch',
-    'label': 'Front-End Punch'
-  },
-  {
-    'value': 'cachedOut',
-    'label': 'Cached Out'
-  },
-  {
-    'value': 'httPapaya',
-    'label': 'httPAPAYA://'
-  },
-  {
-    'value': 'nerdyDaiquiri',
-    'label': 'Nerdy Daiquiri'
-  },
-  {
-    'value': 'theAvernaCode',
-    'label': 'The Averna Code'
-  },
-  {
-    'value': 'focusedTheMostest',
-    'label': 'Focused the Mostest'
-  },
-];
 
 // loadMenu function to add the drinks from the array to the page
-var loadMenu = function() {
-  for (var i = 0; i < cocktails.length; i++) {
-    $('.checkbox-group').append(
-      '<label class="radio" for="' + cocktails[i].value + '"><input type="radio" id="' + cocktails[i].value + '" name="drink" value="' + cocktails[i].value + '">' + cocktails[i].label + '</label>'
-    );
-  }
-};
+
 
 // submitOrder function that increments orderCount, gathers and displays order details.
 var submitOrder = function() {
@@ -71,7 +31,6 @@ var submitOrder = function() {
 
 $(document).ready(function() {
   // call loadMenu function
-  loadMenu();
 
   $('#order-btn').click(function() {
     // call submitOrder function when order button is clicked
