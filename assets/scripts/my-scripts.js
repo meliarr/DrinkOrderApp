5a2b9e658b31ee8b25f14549225d9a430e1e724b
Sarah Duitsman <sksmart9@gmail.com>
2016-07-07 21:47:32 -0500

    Add Worksheet references to file comments

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -1,26 +1,29 @@
-//initialize order count
+// initialize order count (Worksheet Part 1: Step 9a)
 
-/* array of drink objects
+/* array of drink objects (Worksheet Part 2: Step 1)
    each object has a value key with a camelCase drink name value 
    and a label key with the display text for the drink */
 
-// loadMenu function to add the drinks from the array to the page
+// loadMenu function to add the drinks from the array to the page (Worksheet Part 2: Step 2)
 
 
-// submitOrder function that increments orderCount, gathers and displays order details.
+/* submitOrder function that increments orderCount, gathers and displays order details.
+   (Worksheet Part 1: Step 3) */
   
-  // only add orders if there is room left on the order queue
+  // only add orders if there is room left on the order queue (Worksheet Part 1: Step 9g)
 
-  // add order display
+  // add order display (Worksheet Part 1: Step 7)
 
-  // update drink order count
+  // call funciton to update drink order count (Worksheet Part 1: Step 9e)
 
+
+// updateOrderCount function with a parameter passed to it (Worksheet Part 1: Step 9c)
 
 
 $(document).ready(function() {
-  // call loadMenu function
+  // call loadMenu function (Worksheet Part 2: Step 2d)
 
   $('#order-btn').click(function() {
-    // call submitOrder function when order button is clicked
+    // call submitOrder function when order button is clicked (Worksheet Part 1: Step 5b)
   });
 });
