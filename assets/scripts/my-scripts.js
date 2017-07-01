6eb6e9f034c1ac50fd776babf78d1b421fa6288a
Sarah Duitsman <sksmart9@gmail.com>
2017-07-01 01:25:06 -0500

    2017 update

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -1,4 +1,5 @@
 // Initialize orderCount variable to 0 (Worksheet Part 1: Count Orders Step 1
+var orderCount = 0;
 
 /**
 * submitOrder Function
@@ -6,8 +7,18 @@
 * Increment orderCount, gather and display order details.
 * The code from the Worksheet Part 1 Submit an Order Step 2 should go on the line just below the comment close.
 */
-
-  // add order display (Worksheet Part 1: Alerts, Step 2)
+var submitOrder = function () {
+	orderCount++;
+	var orderName = $("#order-form-input").val();
+	console.log(orderName);
+	var drinkName = $("input[type='radio']:checked").val();
+	if(orderCount <= 5) {
+		$("#order-details").append("<h1>" + orderName + " would like a " + drinkName + "</h1>");
+	} else {
+		alert("Drink order queue is full. Please try ordering again in a few minutes.");
+	}
+	updateOrderCount(orderCount);
+};
 
 /**
 * updateOrderCount Function
@@ -16,10 +27,14 @@
 * @param {number} count - counter for orders
 * The code from the Worksheet Part 1: Count Orders, Step 4 should go on the line just below the comment close.
 */
+var updateOrderCount = function (count) {
+	$('#drink-count').html("Drinks Ordered: " + count);
+};
 
 $(document).ready(function() {
 
   $('#order-btn').click(function() {
     // call submitOrder function when order button is clicked (Worksheet Part 1: Call the Function, Step 1)
+		submitOrder();
   });
 });
